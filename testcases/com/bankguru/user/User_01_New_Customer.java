383353d79ba2d7ecc6d23705b3f0b23873e11a3c
OS <OS@DESKTOP-PPOG0J8>
2021-01-16 12:15:05 +0700

    Init new automation test Framework

---
+++ testcases/com/bankguru/user/User_01_New_Customer.java
@@ -0,0 +1,23 @@
+package com.bankguru.user;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class User_01_New_Customer {
+  
+	@BeforeClass
+	 public void beforeClass() {
+	  }
+	
+	
+  @Test
+  public void New() {
+  }
+ 
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+}
---
+++ testdata/com/bankguru/data/payment.java
@@ -0,0 +1,16 @@
+package com.bankguru.data;
+
+public class payment {
+	
+public class FundTransfer{
+	//day la data cho than Fund transfer
+}
+
+public class Deposit{
+	
+}
+
+public class Withdrawal{
+	
+}
+}
